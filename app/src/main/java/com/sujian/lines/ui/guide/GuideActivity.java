13a69e2597ae25681cf8a5691966ce47d57db3b2
sujianqingfeng <121116111@qq.com>
2016-09-05 11:58:23 +0800

    添加闪屏页

---
+++ app/src/main/java/com/sujian/lines/ui/guide/GuideActivity.java
@@ -0,0 +1,69 @@
+package com.sujian.lines.ui.guide;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.sujian.lines.R;
+
+/**
+ * Created by sujian on 2016/9/5.
+ * Mail:121116111@qq.com
+ */
+public class GuideActivity extends AppIntro {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Add your slide's fragments here.
+        // AppIntro will automatically generate the dots indicator and buttons.
+//        addSlide(first_fragment);
+//        addSlide(second_fragment);
+//        addSlide(third_fragment);
+//        addSlide(fourth_fragment);
+
+        // Instead of fragments, you can also use our default slide
+        // Just set a title, description, background and image. AppIntro will do the rest.
+        addSlide(AppIntroFragment.newInstance("1", "1", R.mipmap.app_icon, R.color.buleTextColor));
+        addSlide(AppIntroFragment.newInstance("2", "1", R.mipmap.app_icon, R.color.buleTextColor));
+        addSlide(AppIntroFragment.newInstance("3", "1", R.mipmap.app_icon, R.color.buleTextColor));
+        addSlide(AppIntroFragment.newInstance("4", "1", R.mipmap.app_icon, R.color.buleTextColor));
+
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(false);
+        setProgressButtonEnabled(false);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permisssion in Manifest.
+        setVibrate(true);
+        setVibrateIntensity(30);
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }
+}
--- app/src/main/java/com/sujian/lines/ui/splash/SplashActivity.java
+++ app/src/main/java/com/sujian/lines/ui/splash/SplashActivity.java
@@ -1,10 +1,31 @@
 package com.sujian.lines.ui.splash;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+
+import com.hanks.htextview.HTextView;
+import com.hanks.htextview.HTextViewType;
 import com.sujian.lines.R;
 import com.sujian.lines.base.BaseActivity;
+import com.sujian.lines.ui.guide.GuideActivity;
+
+import butterknife.Bind;
 
 public class SplashActivity extends BaseActivity {
 
+    @Bind(R.id.htv_splash)
+    HTextView htv_splash;
+
+
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            startActivity(new Intent(mContext, GuideActivity.class));
+        }
+    };
 
     @Override
     public int getLayoutId() {
@@ -13,6 +34,9 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        htv_splash.setAnimateType(HTextViewType.LINE);
+        htv_splash.animateText("L i n e s"); // animate
+        handler.sendEmptyMessageDelayed(1,3000);
+        finish();
     }
 }
