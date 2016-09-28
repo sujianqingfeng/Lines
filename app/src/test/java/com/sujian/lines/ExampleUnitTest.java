8b117f60ba65df2a9d917095cbd2f28c653b370e
彭绍国 <pengsg@tembin.com>
2016-09-28 17:46:26 +0800

    编译配置

--- app/src/test/java/com/sujian/lines/ExampleUnitTest.java
+++ app/src/test/java/com/sujian/lines/ExampleUnitTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
@@ -13,7 +14,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
-        System.err.println(String.valueOf(Integer.valueOf(df.format(new Date())) + 1));
+        System.out.println(UUID.randomUUID().toString().replace("-",""));
     }
 }
